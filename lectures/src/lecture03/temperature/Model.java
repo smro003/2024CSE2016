3d2686cf8883511f16a258318053ce44aaaed3a6
Youngjin <smro003@hanyang.ac.kr>
2024-10-03 06:36:36 +0900

    섭씨화씨 변환 완성

--- lectures/src/lecture03/temperature/Model.java
+++ lectures/src/lecture03/temperature/Model.java
@@ -2,6 +2,7 @@ package lecture03.temperature;
 //모델
 public class Model {
     double ctof(double c) {
-        return 0.0;
+
+        return (c * 9/5) + 32 ;
     }
 }
--- lectures/src/lecture03/temperature/View.java
+++ lectures/src/lecture03/temperature/View.java
@@ -1,8 +1,12 @@
 package lecture03.temperature;
 
+import javax.swing.*;
+
 public class View {
     double getTemperature() {
-        return 0.0;
+        String input =JOptionPane.showInputDialog(null,"섭씨 온도를 입력해주세요.");
+        int i=Integer.parseInt(input);
+        return i;
     }
 
     void showTemperature(double c, double f) {
